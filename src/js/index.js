251e0828fd3f32dadf5ee566177f79bd06ca9e15
Dima Devgru <git@devg.ru>
2015-08-18 01:16:31 +0300

    Even more docs!

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,29 @@
-// Файл-заглушка.
-// Используйте навигацию в правом-верхнем углу экрана.
+// Библиотека data-lab-blocks.
+
+// # Блоки
+// Блоки как в БЭМ, переиспользуемые компоненты.
+// * [backedText](/docs/backedText.html) — текст с настраиваемой подложкой;
+// * [dropdownFilter](/docs/backedText.html) — выпадающий фильтр;
+// * [bottomLine](/docs/bottomLine.html) — линия от текстового элемента до низа контейнера.
+
+// # Директивы
+// «Строительные блоки» `angular`-страниц.
+// * [renderLoop](/docs/renderLoop.html) — обеспечивает рассылку `render`-сообщений;
+// * [margin](/docs/margin.html) — сдвигает целевой `SVG`-элемент на отступ, указанный в параметрах `render`;
+// * [sizeToScales](/docs/sizeToScales.html) — задаёт `range` переданным шкалам на основе размеров элемента с учётом отступов.
+
+// # Сервисы
+// Полезные функции.
+// * [multifilter](/docs/multifilter.html) — стандартный фильтр, удобен для проверки массива объектов на соответствие набору свойств;
+// * [numberDeclension](/docs/numberDeclension.html) — склоняет числа;
+// * [removePrototype](/docs/removePrototype.html) — превращает специфичный объект (например, результат `getBBox()`) в обычный;
+// * [roundTo](/docs/roundTo.html) — округляет числа до заданной точности;
+// * [toGrayscale](/docs/toGrayscale.html) — приводит цвет к серому оттенку.
+
+// # Фильтры
+// Доступные в любом `angular`-шаблоне функции.
+// * [preventNaN](/docs/preventNaN.html) — заменяет `NaN` на `undefined`;
+// * [trust](/docs/trust.html) — фильтр для прохода ангуляровской [проверки](https://docs.angularjs.org/api/ng/service/$sce) небезопасного контента;
+// * [services](/docs/services.html) — «превращает» сервисы в фильтры.
+
+// Также используйте навигацию в правом-верхнем углу экрана.
