4de98f0eef8bb1d2c7b7ac74c90178473ba2224d
Dima Devgru <git@devg.ru>
2015-09-01 01:26:17 +0300

    feature(render): apply transition before calling callback

--- src/js/directives/render.js
+++ src/js/directives/render.js
@@ -1,20 +1,25 @@
 // Вызов произвольной функции, обрабатывающей событие `render`.
-angular.module('dataLab').directive('labRender', function () {
+angular.module('dataLab').directive('labRender', function (applyTransition) {
     return {
         // Подключается к элементу, который требуется отрисовать.
         restrict: 'A',
         scope: {
+            // Коллбек передаётся через аргумент с названием директивы.
             cb: '=labRender'
         },
         link: function ($scope, $element) {
+            var element = $element[0];
+            var d3element = d3.select(element);
             // Срабатывает на `render`.
-            $scope.$on('render', function ($event, render) {
+            $scope.$on('render', function ($event, render, transition) {
+                // Автоматически применяет `transition`, анимируя все изменения.
+                var target = applyTransition(d3element, transition);
                 // Коллбек должен быть функцией.
                 if (angular.isFunction($scope.cb))
                 // Коллбек вызывается с аргументами `$event` и `render` — событием с мета-информацией и `render`.
                 // Элемент, на котором используется директива передаётся через `this`.
-                    $scope.cb.call($element[0], $event, render)
-            })
+                    $scope.cb.call(target, $event, render);
+            });
         }
-    }
+    };
 });
