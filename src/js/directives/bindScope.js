6830dd617805429e75658652653e2ae4ca796903
Dima Devgru <git@devg.ru>
2015-09-01 01:24:12 +0300

    feature(bindScope): add bindScope directive

---
+++ src/js/directives/bindScope.js
@@ -0,0 +1,12 @@
+// Помещает `$scope` в `datum` элемента. Даёт доступ к `$scope` в `D3`-обработчиках.
+angular.module('dataLab').directive('labBindScope', function (applyTransition) {
+    return {
+        // Подключается к любому элементу.
+        restrict: 'A',
+        link: function ($scope, $element) {
+            var element = $element[0];
+            var d3element = d3.select(element);
+            d3element.datum($scope);
+        }
+    };
+});
